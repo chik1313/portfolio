2629288e98f6119100a7019eb72fd4c41c2461a5
chik <ochkurov96@gmail.com>
2023-09-26 00:18:42 +0300

    update portfolio

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-            <FlexWrapper align={"center"} justify={"space-between"}>
+            <FlexWrapper align={"center"} justify={"space-around"} wrap={'wrap'}>
                 <div>
                     <SmallText>Hi There</SmallText>
                     <Name>I am <span>Vladyslav Ochkurov </span></Name>
@@ -30,6 +31,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;
   
   &::before{
     content: "";
@@ -41,6 +43,12 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+    }
   }
 `
 
@@ -48,36 +56,46 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
+  
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
-const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
+const MainTitle = styled.h1` 
+  ${font({weight: 400, Fmax: 27, Fmin: 20})}
 `
+
 const Name = styled.h2`
-  font-family: "Josefin Sans",sans-serif;
-  font-size: 50px;
-  font-weight: 700;
+  ${font({family: "'Josefin Sans',sans-serif" , weight: 700 , Fmax: 50, Fmin: 36})};
   letter-spacing: 0.05em;
   margin: 10px 0;
   
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
+    
     &::before {
       content: "";
       display: inline-block;
       width: 100%;
       height: 20px;
       background-color: ${theme.colors.accent};
+      
       position: absolute;
       bottom: 0;
       z-index: -1;
     }
   }
+  
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
 `
 const SmallText = styled.span`
   font-size: 14px;
   font-weight: 400;
- 
 `
 
