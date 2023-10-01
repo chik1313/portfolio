f6d1f1ecfbe48e3e0f1c039caffee1e5ddb0a5e1
chik <ochkurov96@gmail.com>
2023-10-01 18:42:52 +0300

    update portfolio

---
+++ src/components/particle/Particle.tsx
@@ -0,0 +1,88 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+export const Particle = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "grab",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 1,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 200,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 2,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 0.8,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 1000,
+                        },
+                        value: 20,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                            type: "star",
+                    },
+                    size: {
+                        value: { min: 1, max: 3.5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
