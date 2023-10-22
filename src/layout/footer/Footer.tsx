5c8c040ecd1d130161af22cc0f24d1acb381b6e4
chik <ochkurov96@gmail.com>
2023-10-22 16:55:17 +0300

    update portfolio

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -14,7 +14,7 @@ const socialItemData = [
     },
     {
         iconId: "linkedin",
-        link: "https://www.linkedin.com/in/vladyslav-ochkurov-19608a260/s"
+        link: "https://www.linkedin.com/in/vladyslav-ochkurov-19608a260/"
     },
 ]
 export const Footer: React.FC = () => {
