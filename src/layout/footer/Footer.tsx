a64d0f009e3cd19547b369cc476832ac8e4ae13b
chik <ochkurov96@gmail.com>
2023-12-17 23:09:45 +0200

    update portfolio

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -25,7 +25,7 @@ export const Footer: React.FC = () => {
                 <S.SocialList>
                     {socialItemData.map((s, index) => {
                         return <S.SocialItem key={index}>
-                            <S.SocialLink href={s.linlk}>
+                            <S.SocialLink href={s.link}>
                                 <Icon height={'21'} width={'21'} viewBox={'0 0 21 21'} iconId={s.iconId}/>
                             </S.SocialLink>
                         </S.SocialItem>
